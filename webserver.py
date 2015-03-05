c724971cd4de815c5be342ed5b34b18f29940eaf
Aaron Pankratz <pankratz.aaron@gmail.com>
2015-03-04 21:49:09 -0600

    add create restaurant page and post handler

--- webserver.py
+++ webserver.py
@@ -20,7 +20,7 @@ class webserverHandler(BaseHTTPRequestHandler):
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
                 output = ""
-                output += "<html><body>"
+                output += "<html><body><a href='/restaurants/new'>Create a new Restaurant</a></br>"
                 for restaurant in restaurants:
                     output += restaurant.name
                     output += "</br><a href='#'>Edit</a> <a href='#'>Delete</a></br>"
@@ -28,8 +28,37 @@ class webserverHandler(BaseHTTPRequestHandler):
                 self.wfile.write(output)
                 print output
                 return
+            
+            if self.path.endswith("/restaurants/new"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output = ""
+                output += "<html><body><a href='/restaurants'>Back to Restaurants</a></body></html>"
+                output += '''<form method='POST' enctype='multipart/form-data' action='/restaurants/new'><h2>Create a new Restaurant</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+                output += "</body></html>"
+                self.wfile.write(output)
+                print output
+                return
         except IOError:
             self.send_error(404, 'File Not Found: %s' % self.path)
+
+    def do_POST(self):
+        try:
+
+            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+            if ctype == 'multipart/form-data':
+                fields=cgi.parse_multipart(self.rfile, pdict)
+                messagecontent = fields.get('message')
+            restaurant = Restaurant(name=messagecontent[0])
+            session.add(restaurant)
+            session.commit()
+            self.send_response(301)
+            self.send_header('Content-type', 'text/html')
+            self.send_header('Location', '/restaurants')
+            self.end_headers()
+        except:
+            pass
 
 def main():
     try:
