af2a806978e69e2346ea36e0eb06d605d551cdbd
Aaron Pankratz <pankratz.aaron@gmail.com>
2015-03-05 21:12:07 -0600

    add hello path to flask app

--- full-stack-python.py
+++ full-stack-python.py
@@ -1,12 +1,11 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-
 @app.route('/')
+@app.rout('/hello')
 def hello_world():
     return 'Hello World!'
 
-
 if __name__ == '__main__':
+    app.debug = True
     app.run()
