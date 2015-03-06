9f5727d7c56e5f533a4eb61516952e7495a81e60
Aaron Pankratz <pankratz.aaron@gmail.com>
2015-03-05 21:25:18 -0600

    add menu listing

--- full-stack-python.py
+++ full-stack-python.py
@@ -1,10 +1,28 @@
 from flask import Flask
+from database_setup import Base, Restaurant, MenuItem
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/hello')
 def hello_world():
-    return 'Hello World!'
+    restaurant = session.query(Restaurant).first()
+    menu_items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
+    output = ''
+    for menuItem in menu_items:
+        output += menuItem.name
+        output += ' '
+        output += menuItem.price
+        output += '</br>'
+        output += menuItem.description
+        output += '</br></br>'
+    return output
 
 if __name__ == '__main__':
     app.debug = True
