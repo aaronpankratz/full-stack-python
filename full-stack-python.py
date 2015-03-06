82a26dd5cd5c2ba82c2570a0bdceda829dade81f
Aaron Pankratz <pankratz.aaron@gmail.com>
2015-03-05 21:35:38 -0600

    adding edit, new, and delete routes

--- full-stack-python.py
+++ full-stack-python.py
@@ -10,9 +10,9 @@ session = DBSession()
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/hello')
-def hello_world():
-    restaurant = session.query(Restaurant).first()
+@app.route('/restaurants/<int:restaurant_id>/menu/items')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     menu_items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
     output = ''
     for menuItem in menu_items:
@@ -23,6 +23,18 @@ def hello_world():
         output += menuItem.description
         output += '</br></br>'
     return output
+
+@app.route('/restaurants/<int:restaurant_id>/menu/items/new')
+def newMenutItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+@app.route('/restaurants/<int:restaurant_id>/menu/items/<int:menu_id>/edit')
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+@app.route('/restaurants/<int:restaurant_id>/menu/items/<int:menu_id>/delete')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
 
 if __name__ == '__main__':
     app.debug = True
