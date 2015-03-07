68f23ef99894759e50dddbe09ac1334943b1843a
Aaron Pankratz <pankratz.aaron@gmail.com>
2015-03-06 18:54:48 -0600

    add get and post methods for new item

--- full-stack-python.py
+++ full-stack-python.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from database_setup import Base, Restaurant, MenuItem
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -10,21 +10,27 @@ session = DBSession()
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/restaurants/<int:restaurant_id>/menu/items')
+@app.route('/restaurants/<int:restaurant_id>/menu/items/')
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     menu_items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
     return render_template('menu.html', restaurant = restaurant, items = menu_items)
 
-@app.route('/restaurants/<int:restaurant_id>/menu/items/new')
+@app.route('/restaurants/<int:restaurant_id>/menu/items/new/' methods=['GET', 'POST'])
 def newMenutItem(restaurant_id):
-    return "page to create a new menu item. Task 1 complete!"
+    if request.method == 'POST':
+        newItem = MenuItem(name = request.form['name'], restaurant_id=restaurant_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
+    else:
+        return render_template(url_for('restaurantMenu', restaurant_id = restaurant_id)
 
-@app.route('/restaurants/<int:restaurant_id>/menu/items/<int:menu_id>/edit')
+@app.route('/restaurants/<int:restaurant_id>/menu/items/<int:menu_id>/edit/')
 def editMenuItem(restaurant_id, menu_id):
     return "page to edit a menu item. Task 2 complete!"
 
-@app.route('/restaurants/<int:restaurant_id>/menu/items/<int:menu_id>/delete')
+@app.route('/restaurants/<int:restaurant_id>/menu/items/<int:menu_id>/delete/')
 def deleteMenuItem(restaurant_id, menu_id):
     return "page to delete a menu item. Task 3 complete!"
 
